789b68b029dd7bcb76bd655b6126d85cdaee5672
Ronni <ronni_moura@hotmail.com>
2023-04-25 23:03:01 -0300

    ChessException and ChessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -42,10 +42,10 @@ public class Board {
 
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
-			throw new BoardException("There is already a piece on position " + position);
+			throw new BoardException("There is already a piece on position " + position );
 		}
-	pieces[position.getRow()][position.getColumn()]=piece;
-	piece.position=position;
+	pieces[position.getRow()][position.getColumn()] = piece;
+	piece.position = position;
 
 	}
 
